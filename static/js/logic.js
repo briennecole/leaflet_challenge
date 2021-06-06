d4f2d57a44759872fbc152cb11963219c5a83dd1
Brienne <cole.brienne@gmail.com>
2021-06-06 13:14:00 -0700

    ... stuck

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,45 @@
+// Store our API endpoint inside the queryURL
+const queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson"
+
+// Perform a GET request to the query URL
+d3.json(queryUrl).then(data => {
+    console.log(data);
+    console.log(d3.extent(data.features.map(d => d.properties.mag)))
+    // Once we get a response, send the data.features object to the createFeatures function
+    createFeatures(data.features);
+  });
+  
+  function createFeatures(earthquakeData) {
+
+      // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+    function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.title +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    }
+
+      // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+    const earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature,
+     });
+
+    const mags = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature,
+        pointToLayer: (feature, latlng) => {
+        return new L.Circle(latlng, {
+            radius: feature.properties.mag*25000,
+            fillColor: "red",
+            stroke: false 
+        });
+       }
+     });
+
+  // Sending our earthquakes layer to the createMap function
+     createMap(earthquakes, mags);
+}
+
+
 /* NOTE FOR STEP 2
 /  You can use the javascript Promise.all function to make two d3.json calls, 
 /  and your then function will not run until all data has been retreived.
@@ -15,5 +57,4 @@
 /
 /    }).catch(e => console.warn(e));
 /
-/ ----------------------------------------*/
-
+/ ----------------------------------------*/ 
